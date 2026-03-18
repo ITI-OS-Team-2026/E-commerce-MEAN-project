75db65f02a71a4db776d2820e72fc89d07471ed2
elsayedfarg <elsayedfarg792003@gmail.com>
2026-03-19 01:48:44 +0200

    Wrong Routes Handler

--- server/app.js
+++ server/app.js
@@ -18,6 +18,16 @@ app.use(limiter);
 
 // Route Mount
 
+// Handle wrong routes
+app.use((req, res, next) => {
+  try {
+    // We don't have a "resource" for unknown routes, so pass null
+    throwIfNotFound(null, `Route ${req.originalUrl} not found`);
+  } catch (err) {
+    next(err); // Pass error to your global error handler
+  }
+});
+
 // Global error handler
 app.use(errorHandler);
 
