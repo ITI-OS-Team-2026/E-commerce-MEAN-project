2ed6e2847b205b9af38c249de1eb7ee5300029e6
elsayedfarg <elsayedfarg792003@gmail.com>
2026-03-24 15:05:03 +0200

    fix(User routes): Correct the authenticate middleware import

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticate } = require('../middlewares/authenticate.middleware');
+const authenticate = require('../middlewares/authenticate');
 const restrictTo = require('../middlewares/restrictTo');
 const validate = require('../middlewares/validate');
 const schemas = require('../schemas/User');
