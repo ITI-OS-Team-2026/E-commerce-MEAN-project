b190de37be46481a9ced76293105076615473083
elsayedfarg <elsayedfarg792003@gmail.com>
2026-03-24 14:30:27 +0200

    feat(jwt): Add jwt methods inside the jwt file in  the utils folder

---
+++ server/utils/jwt.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const APIError = require('./APIError');
+
+const createJWT = ({ payload }) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFE_TIME,
+  });
+  return token;
+};
+
+const isTokenValid = ({ token }) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    throw new APIError('Invalid or expired token', 401);
+  }
+};
+
+module.exports = {
+  createJWT,
+  isTokenValid,
+};
