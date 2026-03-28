2d44eaa6985695f2917f137ef76ed97aa37bccfb
omarali <omaraliabdelaziz250@gmail.com>
2026-03-28 15:56:15 +0200

    fix: update user ID retrieval in createPaymentIntent and comment out authentication middleware in payment route for testing

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -4,8 +4,8 @@ const APIError = require('../utils/APIError');
 exports.createPaymentIntent = async (req, res, next) => {
   try {
     const { amount, currency, orderId } = req.body;
-    const userId = req.user._id; // from your authenticate middleware
-
+    // const userId = req.user._id; // from your authenticate middleware
+    const userId = req.user?._id || '69c7d03de2f19e2415543c06';
     const result = await paymentService.createPaymentIntent({ amount, currency, userId, orderId });
     res.status(200).json({ status: 'success', data: result });
   } catch (err) {
--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const paymentController = require('../controllers/paymentController.js');
 const authenticate = require('../middlewares/authenticate');
 
-router.post('/create-intent', authenticate, paymentController.createPaymentIntent);
+router.post('/create-intent', /*authenticate*/paymentController.createPaymentIntent);
 router.post('/confirm', authenticate, paymentController.confirmPayment);
 
 module.exports = router;
