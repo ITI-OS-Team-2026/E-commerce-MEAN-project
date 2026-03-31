a91a0d35b199308dbbad4ffb0748e440eab27ad8
Ramadan-Elgamal <ramadanbotsher@gmail.com>
2026-03-31 17:40:15 +0200

    feat: add Navbar and Footer components to product pages and finished their ui

--- client/src/app/features/products/pages/product-details/product-details.ts
+++ client/src/app/features/products/pages/product-details/product-details.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from "../../../../shared/components/footer/footer";
+import { Navbar } from "../../../../shared/components/navbar/navbar";
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [Footer, Navbar],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
