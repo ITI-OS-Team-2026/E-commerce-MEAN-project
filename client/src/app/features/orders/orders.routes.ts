76f95b46307eb0a25dbcb144538b77fc4ba0e92f
Ahmed Azab <ahmed-azab271>
2026-03-31 20:20:05 +0200

    Adding OrderPlacement HTML

--- client/src/app/features/orders/orders.routes.ts
+++ client/src/app/features/orders/orders.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Checkout } from './pages/checkout/checkout';
 import { OrderHistory } from './pages/order-history/order-history';
 import { OrderTrack } from './pages/order-track/order-track';
+import { Placement } from './pages/placement/placement';
 
 export const ORDER_ROUTES: Routes = [
   { path: 'checkout', component: Checkout },
