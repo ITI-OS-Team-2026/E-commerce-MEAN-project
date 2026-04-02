62067c9784ab70f93511caedd85482f61794b592
mohamedahmed-dev <mohamedahmed010320@gmail.com>
2026-04-02 10:57:38 +0200

    fix(payment): fix payment service API URL

--- client/src/app/features/payment/services/payment.service.ts
+++ client/src/app/features/payment/services/payment.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PaymentService {
-  private apiUrl = 'http://localhost:3000/api/payments';
+  private apiUrl = 'http://localhost:5000/api/payments';
 
   constructor(private http: HttpClient) {}
 
