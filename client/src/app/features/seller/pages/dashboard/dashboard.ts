562ca88e923298a4da94e5759218a021367f0a47
Ahmed Azab <ahmed-azab271>
2026-04-02 15:06:38 +0200

    Add Dashboard Component

---
+++ client/src/app/features/seller/pages/dashboard/dashboard.ts
@@ -0,0 +1,65 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InventoryService } from '../../services/inventory.service';
+import { StorageService } from '../../../../core/services/storage.service';
+import { Logo } from '../../../../shared/components/logo/logo';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [CommonModule, Logo , RouterLink],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard implements OnInit {
+  sellerUser: any = {};
+  products: any[] = [];
+  totalProducts = 0;
+  totalRevenue = 0; // Placeholder, calculate from products or orders
+  activeOrders = 0; // Placeholder
+
+  constructor(
+    private inventoryService: InventoryService,
+    private storageService: StorageService,
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.loadSellerInfo();
+    this.loadProducts();
+  }
+
+  loadSellerInfo() {
+    const token = this.storageService.getToken();
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      this.sellerUser = {
+        name: payload.name,
+        userId: payload.userId,
+        role: payload.role,
+        isApproved: payload.isApproved
+      };
+    }
+  }
+
+  loadProducts() {
+    this.inventoryService.getSellerProducts().subscribe({
+      next: (response) => {
+        this.products = response.data.products;
+        this.totalProducts = this.products.length;
+        // Calculate total revenue if price is available
+        this.totalRevenue = this.products.reduce((sum, product) => sum + (product.price || 0), 0);
+        this.cdr.detectChanges()
+      },
+      error: (error) => {
+        console.error('Error loading products', error);
+      }
+    });
+  }
+
+  logout(): void {
+    this.storageService.clearSession();
+    this.router.navigate(['/auth/login']);
+  }
+}
--- client/src/app/features/seller/seller.routes.ts
+++ client/src/app/features/seller/seller.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { Onboarding } from './pages/onboarding/onboarding';
+import { Dashboard } from './pages/dashboard/dashboard';
 import { Inventory } from './pages/inventory/inventory';
 import { ProductEntry } from './pages/product-entry/product-entry';
 
 export const SELLER_ROUTES: Routes = [
-  { path: 'onboarding', component: Onboarding },
+  { path: 'dashboard', component: Dashboard },
   { path: 'inventory', component: Inventory },
   { path: 'productEntry', component: ProductEntry },
 
