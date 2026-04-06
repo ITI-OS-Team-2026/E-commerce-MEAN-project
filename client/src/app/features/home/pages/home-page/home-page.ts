22c2fec4fea2f09f63d1d865fd2f2873852abc0d
elsayedfarg <elsayedfarg792003@gmail.com>
2026-04-06 11:11:32 +0200

    Fixes

--- client/src/app/features/home/pages/home-page/home-page.ts
+++ client/src/app/features/home/pages/home-page/home-page.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeNavbar } from '../../components/home-navbar/home-navbar';
-import { FeaturedCategories } from "../../components/featured-categories/featured-categories";
-import { NewArrivals } from "../../components/new-arrivals/new-arrivals";
-import { FeaturesGrid } from "../../components/features-grid/features-grid";
-import { CtaSection } from "../../components/cta-section/cta-section";
-import { HomeFooter } from "../../components/home-footer/home-footer";
+import { NewArrivals } from '../../components/new-arrivals/new-arrivals';
+import { FeaturesGrid } from '../../components/features-grid/features-grid';
+import { CtaSection } from '../../components/cta-section/cta-section';
+import { HomeFooter } from '../../components/home-footer/home-footer';
 import { Hero } from '../../components/hero/hero';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomeNavbar,Hero, FeaturedCategories, NewArrivals, FeaturesGrid, CtaSection, HomeFooter],
+  imports: [HomeNavbar, Hero, NewArrivals, FeaturesGrid, CtaSection, HomeFooter],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
